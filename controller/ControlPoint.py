66cfd41013039f70a0a1626315d4e84b991a74b0
laholo14 <rtsiory14@gmail.com>
2021-05-11 17:43:49 +0300

    fin

--- controller/ControlPoint.py
+++ controller/ControlPoint.py
@@ -3,84 +3,110 @@ from model.Point import *
 import itertools
 
 
-def temp_2_point(point1, point2):
-    return int(sqrt(pow((point1._get_x() - point2._get_x()), 2) + pow((point1._get_y() - point2._get_y()), 2)))
+class ControlPoint:
 
-def Croissante(L):
-    elementPrecedent = L[0]
-    for element in L:
-        if elementPrecedent > element:
-            return False
-        elementPrecedent = element
-    return True
+    def Decroissante(self, liste):
+        elementPrecedent = liste[0]
+        for element in liste:
+            if elementPrecedent < element:
+                return False
+            elementPrecedent = element
+        # liste[0] = 0
+        # liste.append(0)   
+        return True
 
+    def Pyramide(self, liste, max, taille):
+        elementPrecedent = liste[0]
+        for i, element in enumerate(liste):
+            if element == max:
+                for j in reversed(range(0, i - 1)):
+                    for k in range(0, j + 1):
+                        if liste[k] > liste[k + 1]:
+                            return False
+                for j in reversed(range(i, taille - 1)):
+                    for k in range(i, j + 1):
+                        if liste[k] < liste[k + 1]:
+                            return False
+                # liste[0] = 0
+                # liste.append(0)         
+                return True
+        return False
 
-def Decroissante(L):
-    elementPrecedent = L[0]
-    for element in L:
-        if elementPrecedent < element:
-            return False
-        elementPrecedent = element
-    return True
-    
+    def creation_dico(self, N):
+        dico = {}
+        for i in range(1, N + 1):
+            globals()['point{0}'.format(str(i))] = Point()
+            if str(i) == "1":
+                terme = "ère"
+            else:
+                terme = "ème"
+            globals()['x{0}'.format(str(i))] = int(input("Coordonne x de la " + str(i) + terme + " destination: "))
+            globals()['point{0}'.format(str(i))]._set_x(globals()['x{0}'.format(str(i))])
+            globals()['y{0}'.format(str(i))] = int(input("Coordonne y de la " + str(i) + terme + " destination: "))
+            globals()['point{0}'.format(str(i))]._set_y(globals()['y{0}'.format(str(i))])
+            globals()['z{0}'.format(str(i))] = -1
+            while globals()['z{0}'.format(str(i))] <= 0:
+                globals()['z{0}'.format(str(i))] = int(input("Altitude de la " + str(i) + terme + " destination: "))
+                globals()['point{0}'.format(str(i))]._set_z(globals()['z{0}'.format(str(i))])
+            dico[globals()['point{0}'.format(str(i))]] = globals()['point{0}'.format(str(i))]._get_z()
+        return dico
 
-def Pyramide(L, max, n):
-    elementPrecedent = L[0]
-    for i, element in enumerate(L):
-        if element == max:
-            for j in reversed(range(0, i - 1)):
-                for k in range(0, j + 1):
-                    if L[k] > L[k + 1]:
-                        return False
-            for j in reversed(range(i, n - 1)):
-                for k in range(i, j + 1):
-                    if L[k] < L[k + 1]:
-                        return False
-            return True
-    return False
+    def cas_possible(self, dico, N):
+        tab = []
+        for key in dico.values():
+            tab.append(key)
+        tab.sort(reverse=True)
+        tab_permute = []
+        liste = []
+        valmax = tab[0]
+        tab_permute.extend(list(itertools.permutations(tab)))
 
+        for index, element in enumerate(tab_permute):
+            if (self.Decroissante(element) or not (self.Decroissante(element))) and self.Pyramide(element, valmax, N):
+                # if Decroissante(element) or Croissante(element) or Pyramide(element, valmax, N):
+                liste.append(element)
+        return liste
 
-def cas_possible(dico, N):
-    tab = []
-    for key in dico.values():
-        tab.append(key)
+    def temp_2_point(self, point1, point2):
+        return int(sqrt(pow((point1._get_x() - point2._get_x()), 2) + pow((point1._get_y() - point2._get_y()), 2)))
 
-    tab_permute = []
-    tabfin = []
-    valmax = tab[0]
+    def parcours_0_0(self, liste):
+        for i, element in enumerate(liste):
+            tab = list(element)
+            tab.insert(0, 0)
+            tab.append(0)
+            liste[i] = tab
+        return liste
 
-    tab_permute.extend(list(itertools.permutations(tab)))
+    def calcul(self, liste, dico):
+        tabObject = []
+        point0 = Point()
+        for elementlist in liste:
+            tab = []
+            tab.insert(0, point0)
+            for element in elementlist:
+                for key, valeur in dico.items():
+                    if element == valeur:
+                        tab.append(key)
+            tab.append(point0)
+            tabObject.append(tab)
+        tabSomme = []
+        tabFinal = []
+        for elementObject in tabObject:
+            somme = 0
+            for i in range(0, len(elementObject) - 1):
+                somme += self.temp_2_point(elementObject[i], elementObject[i + 1])
+            tabSomme.append([somme, elementObject])
 
-    for index, element in enumerate(tab_permute):
-        if Decroissante(element) or Croissante(element) or Pyramide(element, valmax, N):
-            tabfin.append(element)
-    return tabfin
+        tempminimalTab = sorted(tabSomme, key=lambda tab: tab[0])
 
+        return tempminimalTab[0]
 
-
-def calcul(N):
-    dico = {}
-    for i in range(1, N + 1):
-        globals()['point{0}'.format(str(i))] = Point()
-        globals()['point{0}'.format(str(i))]._set_id(i)
-        if str(i) == "1":
-            terme = "ère"
-        else:
-            terme = "ème"
-
-        globals()['x{0}'.format(str(i))] = int(input("Coordonne x de la " + str(i) + terme + " destination: "))
-        globals()['point{0}'.format(str(i))]._set_x(globals()['x{0}'.format(str(i))])
-
-        globals()['y{0}'.format(str(i))] = int(input("Coordonne y de la " + str(i) + terme + " destination: "))
-        globals()['point{0}'.format(str(i))]._set_y(globals()['y{0}'.format(str(i))])
-
-        globals()['z{0}'.format(str(i))] = -1
-        while globals()['z{0}'.format(str(i))] <= 0:
-            globals()['z{0}'.format(str(i))] = int(input("Altitude de la " + str(i) + terme + " destination: "))
-            globals()['point{0}'.format(str(i))]._set_z(globals()['z{0}'.format(str(i))])
-
-        dico[globals()['point{0}'.format(str(i))]] = globals()['point{0}'.format(str(i))]._get_z()
-
-    tableau = cas_possible(dico,N)
-    return tableau
-
+    def itineraire(self, tabtemp,N):
+        message = "Veuillez suivre l'itinéraire suivant  "
+        for i,element in enumerate(tabtemp[1]):
+            x = ","
+            if i == N+1:
+                x = "."
+            message += "{}{} ".format(element._get_z(),x)
+        return message + "\nTemps du parcours: {} min".format(tabtemp[0])
--- execute.py
+++ execute.py
@@ -1,11 +1,12 @@
 from controller.ControlPoint import *
 
-
 N = 0
-while N <= 0 or N > 500:
-    N = int(input("Nombre de destinations à desservir(1 à 500): "))
+while N <= 1 or N > 500:
+    N = int(input("Nombre de destinations à desservir(2 à 500): "))
 
+control = ControlPoint()
+dico = control.creation_dico(N)
+itineraire_possible = control.cas_possible(dico,N)
+depart_arrive = control.parcours_0_0(itineraire_possible)
 
-
-print(calcul(N))
-
+print(control.itineraire(control.calcul(depart_arrive,dico),N))
