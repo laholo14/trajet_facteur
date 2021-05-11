9420baed171d1bc2fe3f415387bcaabbe0f94238
laholo14 <rtsiory14@gmail.com>
2021-05-11 22:18:36 +0300

    fichier

--- controller/ControlPoint.py
+++ controller/ControlPoint.py
@@ -1,4 +1,4 @@
-from math import *
+from math import sqrt,pow
 from model.Point import *
 import itertools
 
@@ -11,8 +11,6 @@ class ControlPoint:
             if elementPrecedent < element:
                 return False
             elementPrecedent = element
-        # liste[0] = 0
-        # liste.append(0)   
         return True
 
     def Pyramide(self, liste, max, taille):
@@ -26,50 +24,35 @@ class ControlPoint:
                 for j in reversed(range(i, taille - 1)):
                     for k in range(i, j + 1):
                         if liste[k] < liste[k + 1]:
-                            return False
-                # liste[0] = 0
-                # liste.append(0)         
+                            return False       
                 return True
         return False
 
-    def creation_dico(self, N):
+    def creation_dico(self, fichier):
         dico = {}
-        for i in range(1, N + 1):
-            globals()['point{0}'.format(str(i))] = Point()
-            if str(i) == "1":
-                terme = "ère"
-            else:
-                terme = "ème"
-            globals()['x{0}'.format(str(i))] = int(input("Coordonne x de la " + str(i) + terme + " destination: "))
-            globals()['point{0}'.format(str(i))]._set_x(globals()['x{0}'.format(str(i))])
-            globals()['y{0}'.format(str(i))] = int(input("Coordonne y de la " + str(i) + terme + " destination: "))
-            globals()['point{0}'.format(str(i))]._set_y(globals()['y{0}'.format(str(i))])
-            globals()['z{0}'.format(str(i))] = -1
-            while globals()['z{0}'.format(str(i))] <= 0:
-                globals()['z{0}'.format(str(i))] = int(input("Altitude de la " + str(i) + terme + " destination: "))
-                globals()['point{0}'.format(str(i))]._set_z(globals()['z{0}'.format(str(i))])
-            dico[globals()['point{0}'.format(str(i))]] = globals()['point{0}'.format(str(i))]._get_z()
+        for i,element in enumerate(fichier):
+            globals()['point{}'.format(str(i+1))] = Point()
+            Listsplit = element.split(" ")
+            globals()['point{}'.format(str(i+1))]._set_x(int(Listsplit[0]))
+            globals()['point{}'.format(str(i+1))]._set_y(int(Listsplit[1]))
+            globals()['point{}'.format(str(i+1))]._set_z(int(Listsplit[2]))
+            dico[globals()['point{}'.format(str(i+1))]] = globals()['point{}'.format(str(i+1))]._get_z()
         return dico
 
     def cas_possible(self, dico, N):
         tab = []
+        liste = []
+        tab_permute = []
         for key in dico.values():
             tab.append(key)
         tab.sort(reverse=True)
-        tab_permute = []
-        liste = []
         valmax = tab[0]
         tab_permute.extend(list(itertools.permutations(tab)))
-
-        for index, element in enumerate(tab_permute):
-            if (self.Decroissante(element) or not (self.Decroissante(element))) and self.Pyramide(element, valmax, N):
-                # if Decroissante(element) or Croissante(element) or Pyramide(element, valmax, N):
+        for element in tab_permute:
+            if (self.Decroissante(element) or not(self.Decroissante(element))) and self.Pyramide(element, valmax, N):
                 liste.append(element)
         return liste
-
-    def temp_2_point(self, point1, point2):
-        return int(sqrt(pow((point1._get_x() - point2._get_x()), 2) + pow((point1._get_y() - point2._get_y()), 2)))
-
+   
     def parcours_0_0(self, liste):
         for i, element in enumerate(liste):
             tab = list(element)
@@ -77,9 +60,15 @@ class ControlPoint:
             tab.append(0)
             liste[i] = tab
         return liste
-
+    
+    def temp_2_point(self, point1, point2):
+        return int(sqrt(pow((point1._get_x() - point2._get_x()), 2) + pow((point1._get_y() - point2._get_y()), 2)))
+    
+    
     def calcul(self, liste, dico):
         tabObject = []
+        tabSomme = []
+        tabFinal = []
         point0 = Point()
         for elementlist in liste:
             tab = []
@@ -90,16 +79,12 @@ class ControlPoint:
                         tab.append(key)
             tab.append(point0)
             tabObject.append(tab)
-        tabSomme = []
-        tabFinal = []
         for elementObject in tabObject:
             somme = 0
             for i in range(0, len(elementObject) - 1):
                 somme += self.temp_2_point(elementObject[i], elementObject[i + 1])
             tabSomme.append([somme, elementObject])
-
-        tempminimalTab = sorted(tabSomme, key=lambda tab: tab[0])
-
+        tempminimalTab = sorted(tabSomme, key=lambda index: index[0])
         return tempminimalTab[0]
 
     def itineraire(self, tabtemp,N):
--- execute.py
+++ execute.py
@@ -1,17 +1,20 @@
 from controller.ControlPoint import *
 import time
 
-N = 0
-while N <= 1 or N > 500:
-    N = int(input("Nombre de destinations à desservir(2 à 500): "))
-    
-
+entrer = open("in/1.in","r")
+N = int(entrer.readline())
 control = ControlPoint()
-dico = control.creation_dico(N)
-start_time = time.time()
+dico = control.creation_dico(entrer)
+entrer.close()  
+start_time = time.time() 
 itineraire_possible = control.cas_possible(dico,N)
-depart_arrive = control.parcours_0_0(itineraire_possible)
-
-print(control.itineraire(control.calcul(depart_arrive,dico),N))
-
+print(itineraire_possible)  
 print("--- %s secondes ---" % (time.time() - start_time))
+depart_arrive = control.parcours_0_0(itineraire_possible)
+minimal = control.calcul(depart_arrive,dico)
+
+sortie = open("out/1.out","w")
+sortie.write(control.itineraire(minimal,N))
+sortie.close()
+
+  
